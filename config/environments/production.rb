925927c55cf432905128e3cbbbde3095776b317f
Andrew Ma <4ndrew@gmail.com>
2012-05-03 22:50:45 -0400

    trying to fix heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Todos::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # changed from false -> true in attempt to fix heroku
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
