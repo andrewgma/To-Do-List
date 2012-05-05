d52e227f661c90701551b969d4327e22c63bb4e8
Andrew Ma <4ndrew@gmail.com>
2012-05-05 14:12:32 -0400

    task controller fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,7 +37,7 @@ class TasksController < ApplicationController
 
   # GET /tasks/1/edit
   def edit
-    @task = current_user.tasks.find(params[:id])
+    @task = current_user.tasks.find(params[:task])
   end
 
   # POST /tasks
